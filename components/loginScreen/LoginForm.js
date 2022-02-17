daa298b28e6ab699ce1e62eca4a6157049618daa
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-02-17 23:25:06 +0500

    Adding inputs for Login Form component

--- components/loginScreen/LoginForm.js
+++ components/loginScreen/LoginForm.js
@@ -1,14 +1,57 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 
 const LoginForm = () => {
   return (
-    <View>
-      <Text>LoginForm</Text>
+    <View style={styles.wrapper}>
+      <View style={styles.inputField}>
+        <TextInput 
+          placeholder='Phone number, username or email'
+          placeholderTextColor='#444'
+          autoCapitalize='none'
+          keyboardType='email-address'
+          textContentType='emailAddress'
+          autoFocus={true}
+        />
+      </View>
+      <View style={styles.inputField}>
+        <TextInput 
+          placeholder='Password'
+          placeholderTextColor='#444'
+          autoCapitalize='none'
+          autoCorrect={false}
+          secureTextEntry={true}
+          textContentType='password'
+        />
+      </View>
+      
     </View>
   )
 }
 
 export default LoginForm
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  wrapper: {
+    marginTop: 80,
+  },
+  inputField: {
+    borderRadius: 4,
+    padding: 12,
+    backgroundColor: '#FAFAFA',
+    marginBottom: 10,
+    borderWidth: 1,
+  },
+  button: {
+    backgroundColor: '#0096F6',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: 42,
+    borderRadius: 4,
+  },
+  buttonText: {
+    fontWeight: '600',
+    color: '#fff',
+    fontSize: 18,
+  }
+})
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,6 @@
 import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import LoginForm from '../components/loginScreen/LoginForm'
 
 const INSTAGRAM_LOGO = 'https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Instagram_colored_svg_1-512.png'
 
@@ -9,6 +10,7 @@ const LoginScreen = () => {
       <View style={styles.logoContainer}>
         <Image source={{uri: INSTAGRAM_LOGO, height: 100, width: 100}}/>
       </View>
+      <LoginForm/>
     </View>
   )
 }
